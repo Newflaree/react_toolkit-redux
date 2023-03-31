ccb4ef28aac4fde28e603bd7569f719e2e902816
Camilo López <camiloj.lopez79@gmail.com>
2023-03-31 11:45:52 -0300

    pokemonSlice

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,16 @@
+const PokemonApi = () => {
+  return (
+    <>
+      <h1>PokemonApp</h1>
+      <hr />
+      
+      <ul>
+        <li>Hola</li>
+        <li>Hola</li>
+        <li>Hola</li>
+      </ul>
+    </>
+  );
+}
+
+export default PokemonApi;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './App';
+import PokemonApp from './PokemonApp';
 import './index.css';
 
 import { store } from './store/store';
@@ -10,7 +10,7 @@ import { store } from './store/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={ store }>
-      <App />
+      <PokemonApp />
     </Provider>
   </React.StrictMode>,
 );
